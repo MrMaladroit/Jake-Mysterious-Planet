56720d130b21fac2a1c1eff90650b50e74ed7a0d
Jim Beck <jamesbeckjr@gmail.com>
2018-08-29 14:43:40 -0400

    Added Basic functionality to GameManager

--- Jake-Mysterious-Planet/Assets/Scripts/GameManager.cs
+++ Jake-Mysterious-Planet/Assets/Scripts/GameManager.cs
@@ -1,19 +1,55 @@
 ﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
 
 public class GameManager : MonoBehaviour
 {
+    public GameState currentGameState = GameState.menu;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
     public void StartGame()
     {
-        
+        SetGameState(GameState.inGame);
     }
 
     public void GameOver()  
     {
-        
+        SetGameState(GameState.gameOver);
     }
 
     public void BackToMenu()
     {
+        SetGameState(GameState.menu);
+    }
 
+    private void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+
+        }
+        else if(newGameState == GameState.inGame)
+        {
+
+        }
+        else if(newGameState == GameState.gameOver)
+        {
+            
+        }
     }
 }
