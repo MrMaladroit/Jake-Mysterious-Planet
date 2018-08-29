761f00b29b8a558bd57c963cabdc091bdf3594e5
Jim Beck <jamesbeckjr@gmail.com>
2018-08-29 14:00:34 -0400

    Added Jump Functionality
    Added proper jumping functionality to Jake so he ignores clicks if he isn't ground.

--- Jake-Mysterious-Planet/Assets/Scripts/PlayerController.cs
+++ Jake-Mysterious-Planet/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float jumpForce = 25f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public LayerMask groundLayer;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Left Mouse button clicked!");
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if(isGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        
+    }
+
+    private bool isGrounded()
+    {
+        if(Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
