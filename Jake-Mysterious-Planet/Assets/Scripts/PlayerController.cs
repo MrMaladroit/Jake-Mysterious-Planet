c7fed01a5ac427c45d2d8103dfcf4167a93888a1
Jim Beck <jamesbeckjr@gmail.com>
2018-08-29 13:28:51 -0400

    Add PlayerController.cs
    Saved the current scene
    Added PlayerController.cs to Player object

---
+++ Jake-Mysterious-Planet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Mouse button clicked!");
+        }
+    }
+}
