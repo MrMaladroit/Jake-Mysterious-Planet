ef7305d419bc2b8650580ae90cb2ecc459c4b1cf
Jim Beck <jamesbeckjr@gmail.com>
2018-08-29 14:25:38 -0400

    Added Running Functionality
    Jake runs across the screen at a pace of 1.5f

--- Jake-Mysterious-Planet/Assets/Scripts/PlayerController.cs
+++ Jake-Mysterious-Planet/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float jumpForce = 25f;
+    [SerializeField]
+    private float runningSpeed = 1.5f;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rigidBody;
     private Animator animator;
 
     public LayerMask groundLayer;
@@ -14,22 +16,37 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
     }
+
+    private void Start()
+    {
+        animator.SetBool("isAlive", true);
+    }
+
+    private void FixedUpdate()
+    {
+        if(rigidBody.velocity.x < runningSpeed)
+        {
+            rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+        }
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Jump();
         }
+        animator.SetBool("isGrounded", isGrounded());
     }
 
     private void Jump()
     {
         if(isGrounded())
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
         
     }
