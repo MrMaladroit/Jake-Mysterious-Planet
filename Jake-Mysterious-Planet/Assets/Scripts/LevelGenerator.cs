7835bc76aafe6cd33ef78bb48e80cda8c0c2b684
Jim Beck <jamesbeckjr@gmail.com>
2018-08-29 21:22:27 -0400

    Infinite Level Generator
    Added Infinite Level generation
    Imported CameraFollow.unitypackage
    Added LeaveTrigger.cs

---
+++ Jake-Mysterious-Planet/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public static LevelGenerator instance;
+    // list of all possible chunks of level that can be copied and added to the level
+    public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+
+
+    public Transform levelStartPoint;
+    // list of all levelPrefabs that currently exist in the level
+    public List<LevelPiece> pieces = new List<LevelPiece>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        GenerateInitialPieces();
+    }
+
+    private void GenerateInitialPieces()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            AddPiece();
+        }
+    }
+
+    public void AddPiece()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, levelPrefabs.Count);
+
+        LevelPiece piece = (LevelPiece)Instantiate(levelPrefabs[randomIndex]);
+        piece.transform.SetParent(this.transform, false);
+
+        Vector3 spawnPosition = Vector3.zero;
+
+        if (pieces.Count == 0)
+        {
+            spawnPosition = levelStartPoint.position;
+        }
+        else
+        {
+            spawnPosition = pieces[pieces.Count - 1].exitPoint.position;
+        }
+        piece.transform.position = spawnPosition;
+        pieces.Add(piece);
+    }
+
+    public void RemoveOldestPiece()
+    {
+        LevelPiece oldestpiece = pieces[0];
+
+        pieces.Remove(oldestpiece);
+        Destroy(oldestpiece.gameObject);
+    }
+}
