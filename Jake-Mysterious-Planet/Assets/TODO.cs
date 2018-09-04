4caa00be6c0eedb8e630396510d0e75e79ee5e01
Jim Beck <jamesbeckjr@gmail.com>
2018-09-04 18:01:33 -0400

    Built Android 0.1 and created TODO

---
+++ Jake-Mysterious-Planet/Assets/TODO.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * 
+ *          THIS IS THE TO-DO LIST FOR JAKE (FROM STATEFARM) ON THE MYSTERIOUS PLANET
+ *          
+ *          
+ *      -- Add level music
+ *      -- Add Sound FX for things like jumping, collecting coins, dying
+ *      -- Finish Rock obstacle graphics
+ *      -- Revamp LevelGenerator with added features:
+ *              -- More level chunk variety
+ *              -- Set certain level chunks to be used for the beginning only
+ *              -- Fix current level chunks to spawn coins in better positions
+ *      -- Add a in-game store with features:
+ *              -- Batteries for the rocket
+ *              -- Parts for the rocket
+ *              -- Level Packs
+ *          
+ *      -- Once the player has bought all the parts the final level is unlocked
+ *      -- Add more obstacles, maybe add rolling and spikes
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
